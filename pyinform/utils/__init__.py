486be301efd3a92394c0d829d421f3c689f77265
Douglas Moore <doug@dglmoore.com>
2016-07-24 23:11:31 -0400

    Wrap the inform_coalesce function

--- pyinform/utils/__init__.py
+++ pyinform/utils/__init__.py
@@ -2,3 +2,4 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 from pyinform.utils.binning import *
+from pyinform.utils.coalesce import *
--- test/test_utils.py
+++ test/test_utils.py
@@ -132,13 +132,38 @@ class TestBinning(unittest.TestCase):
 
     def test_bin_2D(self):
         binned, _, _ = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], b=3)
-        self.assertTrue((np.asarray([[0,0,1,1,2,2],[2,2,1,1,0,0]]) == binned).all())
+        self.assertTrue(([[0,0,1,1,2,2],[2,2,1,1,0,0]] == binned).all())
 
         binned, _, _ = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], step=2.0)
-        self.assertTrue((np.asarray([[0,0,1,1,2,2],[2,2,1,1,0,0]]) == binned).all())
+        self.assertTrue(([[0,0,1,1,2,2],[2,2,1,1,0,0]] == binned).all())
 
         binned, _, _ = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], bounds=[2.5, 4.5])
-        self.assertTrue((np.asarray([[0,0,1,1,2,2],[2,2,1,1,0,0]]) == binned).all())
+        self.assertTrue(([[0,0,1,1,2,2],[2,2,1,1,0,0]] == binned).all())
+
+class TestCoalesce(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(InformError):
+            coalesce_series([])
+
+    def test_unchanged(self):
+        series = [0,2,2,1,2,3]
+        coal, b = coalesce_series(series)
+        self.assertEqual(4, b)
+        self.assertTrue((series == coal).all())
+    
+    def test_shifted(self):
+        series = [1,3,3,2,3,4]
+        expect = [0,2,2,1,2,3]
+        coal, b = coalesce_series(series)
+        self.assertEqual(4, b)
+        self.assertTrue((expect == coal).all())
+
+    def test_no_gaps(self):
+        series = [2,8,7,2,0,0]
+        expect = [1,3,2,1,0,0]
+        coal, b = coalesce_series(series)
+        self.assertEqual(4, b)
+        self.assertTrue((expect == coal).all())
 
 if __name__ == "__main__":
     unittest.main()
