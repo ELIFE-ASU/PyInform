f222566ac0df9fb7fe05c931091986f90821c618
Douglas Moore <doug@dglmoore.com>
2016-07-24 22:18:45 -0400

    Create bin_series
    bin_series wraps the various binning functions
    provided by inform. However, at present only
    inform_bin (fixed number of uniform bins) is wrapped. More to come.

--- pyinform/utils/binning.py
+++ pyinform/utils/binning.py
@@ -13,11 +13,34 @@ def range(series):
     """
     xs = np.asarray(series, dtype=np.float64)
     data = xs.ctypes.data_as(POINTER(c_double))
+
     min, max = c_double(), c_double()
+    
     e = ErrorCode(0)
     rng = _inform_range(data, c_ulong(len(xs)), byref(min), byref(max), byref(e))
     error_guard(e)
+    
     return rng, min.value, max.value
+
+def bin_series(series, b):
+    """
+    Bin a continuously-valued timeseries into `b` uniform bins
+    """
+    xs = np.asarray(series, dtype=np.float64)
+    shape = xs.shape
+    data = xs.ravel().ctypes.data_as(POINTER(c_double))
+
+    binned = np.empty(xs.size, dtype=np.int32)
+    out = binned.ctypes.data_as(POINTER(c_int))
+
+    e = ErrorCode(0)
+    step = _inform_bin(data, c_ulong(xs.size), c_int(b), out, byref(e))
+    error_guard(e)
+
+    binned = np.reshape(binned, shape)
+
+    return binned, step
+
 
 _inform_range = _inform.inform_range
 _inform_range.argtypes = [POINTER(c_double), c_ulong, POINTER(c_double), POINTER(c_double), POINTER(c_int)]
--- test/test_utils.py
+++ test/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 from pyinform.utils import *
 from pyinform.error import InformError
 
-class Range(unittest.TestCase):
+class TestRange(unittest.TestCase):
     def test_null_series(self):
         with self.assertRaises(InformError):
             range([])
@@ -20,6 +20,43 @@ class Range(unittest.TestCase):
         self.assertAlmostEqual(0.0, rng)
         self.assertAlmostEqual(1.0, min)
         self.assertAlmostEqual(1.0, max)
+
+class TestBinning(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(InformError):
+            bin_series([], b=2)
+
+    def test_invalid_binning(self):
+        with self.assertRaises(InformError):
+            bin_series([1,2,3,4,5,6], b=-1)
+
+        with self.assertRaises(InformError):
+            bin_series([1,2,3,4,5,6], b=0)
+
+        with self.assertRaises(InformError):
+            bin_series([1,2,3,4,5,6], b=1)
+
+    def test_two_bins(self):
+        binned, step = bin_series([1,2,3,4,5,6], b=2)
+        self.assertAlmostEqual(5/2, step)
+        for i, x in enumerate([0,0,0,1,1,1]):
+            self.assertEqual(x, binned[i])
+
+    def test_three_bins(self):
+        binned, step = bin_series([1,2,3,4,5,6], b=3)
+        self.assertAlmostEqual(5/3, step)
+        for i, x in enumerate([0,0,1,1,2,2]):
+            self.assertEqual(x, binned[i])
+
+    def test_six_bins(self):
+        binned, step = bin_series([1,2,3,4,5,6], b=6)
+        self.assertAlmostEqual(5/6, step)
+        for i, x in enumerate([0,1,2,3,4,5]):
+            self.assertEqual(x, binned[i])
+
+    def test_bin_2D(self):
+        binned, step = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], b=3)
+        self.assertTrue((np.asarray([[0,0,1,1,2,2],[2,2,1,1,0,0]]) == binned).all())
 
 if __name__ == "__main__":
     unittest.main()
