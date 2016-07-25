1b36b705d31483f9614f95600c1c9d6141ee9d46
Douglas Moore <doug@dglmoore.com>
2016-07-24 22:43:44 -0400

    Extend bin_series to bin to specified step size

--- pyinform/utils/binning.py
+++ pyinform/utils/binning.py
@@ -22,10 +22,15 @@ def range(series):
     
     return rng, min.value, max.value
 
-def bin_series(series, b):
+def bin_series(series, b=None, step=None):
     """
     Bin a continuously-valued timeseries into `b` uniform bins
     """
+    if b is None and step is None:
+        raise ValueError("must provide either number of bins or step size")
+    elif b is not None and step is not None:
+        raise ValueError("must provide either number of bins or step size")
+    
     xs = np.asarray(series, dtype=np.float64)
     shape = xs.shape
     data = xs.ravel().ctypes.data_as(POINTER(c_double))
@@ -34,14 +39,26 @@ def bin_series(series, b):
     out = binned.ctypes.data_as(POINTER(c_int))
 
     e = ErrorCode(0)
-    step = _inform_bin(data, c_ulong(xs.size), c_int(b), out, byref(e))
+    if b is not None:
+        spec = _inform_bin(data, c_ulong(xs.size), c_int(b), out, byref(e))
+    elif step is not None:
+        spec = step
+        b = _inform_bin_step(data, c_ulong(xs.size), c_double(step), out, byref(e))
     error_guard(e)
 
     binned = np.reshape(binned, shape)
 
-    return binned, step
+    return binned, b, spec
 
 
 _inform_range = _inform.inform_range
 _inform_range.argtypes = [POINTER(c_double), c_ulong, POINTER(c_double), POINTER(c_double), POINTER(c_int)]
 _inform_range.restype = c_double
+
+_inform_bin = _inform.inform_bin
+_inform_bin.argtypes = [POINTER(c_double), c_ulong, c_int, POINTER(c_int), POINTER(c_int)]
+_inform_bin.restype = c_double
+
+_inform_bin_step = _inform.inform_bin_step
+_inform_bin_step.argtypes = [POINTER(c_double), c_ulong, c_double, POINTER(c_int), POINTER(c_int)]
+_inform_bin_step.restype = c_int
--- test/test_utils.py
+++ test/test_utils.py
@@ -22,9 +22,19 @@ class TestRange(unittest.TestCase):
         self.assertAlmostEqual(1.0, max)
 
 class TestBinning(unittest.TestCase):
+    def test_keyword_args(self):
+        with self.assertRaises(ValueError):
+            bin_series([1,2,3,4])
+
+        with self.assertRaises(ValueError):
+            bin_series([1,2,3,4], b=2, step=2)
+
     def test_empty(self):
         with self.assertRaises(InformError):
             bin_series([], b=2)
+
+        with self.assertRaises(InformError):
+            bin_series([], step=2)
 
     def test_invalid_binning(self):
         with self.assertRaises(InformError):
@@ -36,26 +46,59 @@ class TestBinning(unittest.TestCase):
         with self.assertRaises(InformError):
             bin_series([1,2,3,4,5,6], b=1)
 
+        with self.assertRaises(InformError):
+            bin_series([1,2,3,4,5,6], step=-1)
+
+        with self.assertRaises(InformError):
+            bin_series([1,2,3,4,5,6], step=0)
+
     def test_two_bins(self):
-        binned, step = bin_series([1,2,3,4,5,6], b=2)
+        binned, _, step = bin_series([1,2,3,4,5,6], b=2)
         self.assertAlmostEqual(5/2, step)
         for i, x in enumerate([0,0,0,1,1,1]):
             self.assertEqual(x, binned[i])
 
     def test_three_bins(self):
-        binned, step = bin_series([1,2,3,4,5,6], b=3)
+        binned, _, step = bin_series([1,2,3,4,5,6], b=3)
         self.assertAlmostEqual(5/3, step)
         for i, x in enumerate([0,0,1,1,2,2]):
             self.assertEqual(x, binned[i])
 
     def test_six_bins(self):
-        binned, step = bin_series([1,2,3,4,5,6], b=6)
+        binned, _, step = bin_series([1,2,3,4,5,6], b=6)
         self.assertAlmostEqual(5/6, step)
         for i, x in enumerate([0,1,2,3,4,5]):
             self.assertEqual(x, binned[i])
 
+    def test_size_two(self):
+        binned, b, _ = bin_series([1,2,3,4,5,6], step=2.0)
+        self.assertEqual(3, b)
+        for i, x in enumerate([0,0,1,1,2,2]):
+            self.assertEqual(x, binned[i])
+
+    def test_size_fivehalves(self):
+        binned, b, _ = bin_series([1,2,3,4,5,6], step=5/2)
+        self.assertEqual(3, b)
+        for i, x in enumerate([0,0,0,1,1,2]):
+            self.assertEqual(x, binned[i])
+
+    def test_size_one(self):
+        binned, b, _ = bin_series([1,2,3,4,5,6], step=1)
+        self.assertEqual(6, b)
+        for i, x in enumerate([0,1,2,3,4,5]):
+            self.assertEqual(x, binned[i])
+
+    def test_size_half(self):
+        binned, b, _ = bin_series([1,2,3,4,5,6], step=0.5)
+        self.assertEqual(11, b)
+        for i, x in enumerate([0,2,4,6,8,10]):
+            self.assertEqual(x, binned[i])
+
     def test_bin_2D(self):
-        binned, step = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], b=3)
+        binned, _, _ = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], b=3)
+        self.assertTrue((np.asarray([[0,0,1,1,2,2],[2,2,1,1,0,0]]) == binned).all())
+
+        binned, _, _ = bin_series([[1,2,3,4,5,6], [6,5,4,3,2,1]], step=2.0)
         self.assertTrue((np.asarray([[0,0,1,1,2,2],[2,2,1,1,0,0]]) == binned).all())
 
 if __name__ == "__main__":
