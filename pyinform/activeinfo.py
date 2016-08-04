39812c6d3a558aebb151e67ab92ba70fe15bda97
Douglas Moore <doug@dglmoore.com>
2016-08-03 22:39:08 -0700

    Create basic documentation for active_info

--- pyinform/activeinfo.py
+++ pyinform/activeinfo.py
@@ -9,7 +9,42 @@ from pyinform.error import ErrorCode, error_guard
 
 def active_info(series, k, b=0, local=False):
     """
-    Compute the active information of a timeseries
+    Compute the (local) active information of a timeseries with history length
+    *k*.
+
+    Active information was introduced in [Lizier2012]_ to quantify information
+    storage in distributed computation. If :math:`\\{x_i\\}_{i \\geq 0}` are time
+    series data of a discrete-time process and :math:`x^k_i` is the
+    :math:`k`-length history at timestep :math:`i`, the local active information
+    of the process is defined by
+
+    .. math::
+
+        A_x(k) &= \sum_{x^k_i,\, x_{i+1}} p(x^k_i, x_{i+1}) \\log_b \\frac{p(x^k_i, x_{i+1})}{p(x^k_i)p(x_{i+1})}.
+    
+    Examples: ::
+
+        >>> active_info([0,0,1,1,1,1,0,0,0], k=2)
+        0.3059584928680419
+
+        >>> active_info([0,0,1,1,1,1,0,0,0], k=2, b=3)
+        0.19303831650832826
+
+        >>> active_info([0,0,1,1,1,1,0,0,0], k=2, local=True)
+        array([[-0.19264508,  0.80735492,  0.22239242,  0.22239242, -0.36257008,
+                1.22239242,  0.22239242]])
+
+    :param series: the time series
+    :type series: sequence or `numpy.ndarray`
+    :param int k: the history length
+    :param int b: the base of the time series and logarithm
+    :param bool local: compute the local active information
+    :returns: the average or local active information
+    :rtype: float or `numpy.ndarray`
+
+    .. [Lizier2012] J.T. Lizier, M. Prokopenko and A.Y. Zomaya, "`Local measures of information storage in complex distributed computation`__" Information Sciences, vol. 208, pp. 39-54, 2012.
+
+    .. __: http://dx.doi.org/10.1016/j.ins.2012.04.016
     """
     xs = np.ascontiguousarray(series, np.int32)
 
